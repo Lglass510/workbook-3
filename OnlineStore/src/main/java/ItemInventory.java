fa4802f2989b216c87fc6acccbe81fb529f3deee
Lourie Glass <lourieglass@gmail.com>
2025-04-25 12:00:44 -0500

    Remvoed fields from Item Inventory

---
+++ OnlineStore/src/main/java/ItemInventory.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+
+public class ItemInventory {
+
+
+    public static void displayProducts(String filename) {
+
+
+        try {
+
+        /*Using file input stream to point to a specific file
+        than creating my scanner to reference the file I want read
+         */
+            FileInputStream fis = new FileInputStream("src\\main\\resources\\products.csv");
+            Scanner sc = new Scanner(fis);
+            String input;
+
+            /*Read all data in file and
+            separate the lines and display the input*/
+            while (sc.hasNextLine()) {
+                input = sc.nextLine();
+                System.out.println(input);
+            }
+            //closing my scanner
+            sc.close();
+
+            //Display message if unable to read file;
+        } catch(IOException e) {
+            System.out.println("Unable to read file");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        displayProducts("products.csv");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
